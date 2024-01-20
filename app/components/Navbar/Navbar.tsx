80e503bce82390031569551f39392f7ddf4d14c6
Chaffexd <shanechaffe@gmail.com>
2024-01-20 12:08:06 +0000

    link to home from nav

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <nav className="w-full bg-[url('/assets/desktop/bg-pattern-header.svg')] h-44 bg-no-repeat bg-cover">
-        <div className="flex justify-between px-36 pt-12 h-full">
-            <h1 className="text-white text-4xl">devjobs</h1>
-            <p className="text-white text-2xl">Day/Night Toggle</p>
-        </div>
+      <div className="flex justify-between px-36 pt-12 h-full">
+        <Link href={"/"}>
+          <h1 className="text-white text-4xl">devjobs</h1>
+        </Link>
+        <p className="text-white text-2xl">Day/Night Toggle</p>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
